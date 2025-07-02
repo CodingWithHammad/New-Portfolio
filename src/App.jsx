220dc4f226a82ab7be32b6dac4cdeefa4c349141
CodingWithHammad <codingwithhammad786@gmail.com>
2025-07-02 22:59:46 +0530

    add and do some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos similique dolor numquam minima doloremque odit tempora, corporis dicta nisi id veniam est, laborum, eligendi voluptates necessitatibus modi minus nostrum facilis?
       </div>
       <div>
-        it's hain bhai kese hain badiga hoio bjhai
+        it&apos;s hain bhai kese hain badiga hoio bjhai
       </div>
     </>
   )
