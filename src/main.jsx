f377943d020ff2cca208d11bbd0f0a294c39e52c
CodingWithHammad <codingwithhammad786@gmail.com>
2025-07-04 22:20:16 +0530

    Added new files such as About,skill,Education etc for my portfolio and do basic work on each

--- src/main.jsx
+++ src/main.jsx
@@ -10,3 +10,19 @@ createRoot(document.getElementById('root')).render(
 )
 
 
+
+
+
+
+
+
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+  <App />
+
+)
